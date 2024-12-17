0007dc1712b0e3dd6ce26761bd61d85404a71ec9
stropee <stropee@campus-eni.fr>
2024-12-17 16:47:44 +0100

    Module 6 - demo 1 partie 1

---
+++ lib/mod6/demo1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(PasswordInput());
+}
+
+class PasswordInput extends StatefulWidget{
+  @override
+  State<PasswordInput> createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
