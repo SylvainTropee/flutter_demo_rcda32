0de85a123b4e160aecd7b7cce46288f1f2dc3a47
stropee <stropee@campus-eni.fr>
2024-12-18 09:43:17 +0100

    Module 6 - demo 1 fin

--- lib/mod6/demo1.dart
+++ lib/mod6/demo1.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(PasswordInput());
 }
 
-class PasswordInput extends StatefulWidget{
+class PasswordInput extends StatefulWidget {
   @override
   State<PasswordInput> createState() => _PasswordInputState();
 }
 
 class _PasswordInputState extends State<PasswordInput> {
+  bool _isVisible = false;
+
+  void changeVisibility(bool value) {
+    setState(() {
+      _isVisible = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+        title: "Démo state",
+        home: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  obscureText: !_isVisible,
+                  decoration: InputDecoration(labelText: "Mot de passe"),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(_isVisible ? Icons.lock_open : Icons.lock),
+                    Switch(
+                        value: _isVisible,
+                        onChanged: changeVisibility
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
